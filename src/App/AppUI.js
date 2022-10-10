6ee63fe25a357770266efaaf7c653a270037d70e
srisposi <sdrisposi@gmail.com>
2022-10-10 12:57:23 -0300

    add form on modal

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -6,6 +6,7 @@ import { TodoItem } from "../components/TodoItem";
 import { CreateTodoButton } from "../components/CreateTodoButton";
 import { TodoContext } from "../components/TodoContext";
 import { Modal } from "../components/Modal";
+import { TodoForm } from "../components/TodoForm";
 
 function AppUI() {
   const {
@@ -41,7 +42,7 @@ function AppUI() {
 
       {!!openModal && (
         <Modal>
-          <p>{searchedTodos[0]?.text}</p>
+          <TodoForm />
         </Modal>
       )}
 
--- src/components/TodoContext/index.js
+++ src/components/TodoContext/index.js
@@ -35,6 +35,15 @@ function TodoProvider(props) {
     saveTodos(newTodos);
   };
 
+  const addTodo = (text) => {
+    const newTodos = [...todos];
+    newTodos.push({
+      completed: false,
+      text:text,
+  }) = true;
+    saveTodos(newTodos);
+  };
+
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
@@ -52,6 +61,7 @@ function TodoProvider(props) {
         setSearchValue,
         searchedTodos,
         completeTodo,
+        addTodo,
         deleteTodo,
         openModal,
         setOpenModal,
